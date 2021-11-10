427379dd2aaed98a91585abb23f8980b4e20e2ac
Junior da Silva <jr.a.silva@outlook.com>
2021-11-09 21:13:42 -0300

    Random characters

--- src/App.js
+++ src/App.js
@@ -11,16 +11,33 @@ function App() {
       .then((response) => setCharacterList(response));
   }, []);
 
-  let filteredCharacterList = characterList
+  let filteredCharacterList01 = characterList
     .filter((item) => item.image !== "")
     .sort(() => Math.random() - 0.5)
-    .slice(0, 3);
+    .filter((item) => item.house === "Gryffindor")
+    .slice(0, 1);
+
+  let filteredCharacterList02 = characterList
+    .filter((item) => item.image !== "")
+    .sort(() => Math.random() - 0.5)
+    .filter((item) => item.house === "Slytherin")
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 1);
+
+  let filteredCharacterList03 = characterList
+    .filter((item) => item.image !== "")
+    .sort(() => Math.random() - 0.5)
+    .filter((item) => item.house === "Ravenclaw")
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 1);
 
   return (
     <div className="App">
       <h1>Harry Potter</h1>
       <main>
-        <CharCard characterList={filteredCharacterList} />
+        <CharCard characterList={filteredCharacterList01} />
+        <CharCard characterList={filteredCharacterList02} />
+        <CharCard characterList={filteredCharacterList03} />
       </main>
     </div>
   );
